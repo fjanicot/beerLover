d7c938302f16231d4e773d0888596391f263ca89
Floriane Janicot <janicot.floriane@gmail.com>
2022-07-06 15:22:47 +0200

    Basic style (header, footer, body and normalize)

--- template/layout.php
+++ template/layout.php
@@ -3,9 +3,53 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/main.css" rel="stylesheet"/>
         <title><?php echo $config['title'] ?? 'BeerLover'; ?></title>
     </head>
     <body>
+        <header>
+            <a href="#"><img src="assets/img/logo.png" alt="Logo de Beer Lover, votre référence houblonnée !"/></a>
+            <div><a href="#">Se connecter</a>
+            <nav>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Nos bières</a></li>
+                    <li><a href="#">À propos</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav></div>
+        </header>
+
+    <main>
         <?php require $config['path']; ?>
+    </main>
+
+    <footer>
+        <div><ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Nos bières</a></li>
+                <li><a href="#">À propos</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div>
+                <p>BeerLover</p>
+                <p>Adresse</p>
+                <p>59000 Lille</p>
+            </div>
+            <div class="social">
+                <a href="#">
+                    <img src="assets/img/icons/instagram.png" alt="Logo Instagram">
+                </a>
+                <a href="#">
+                    <img src="assets/img/icons/twitter.png" alt="Logo Twitter">
+                </a>
+                <a href="#">
+                    <img src="assets/img/icons/facebook.png" alt="Logo Facebook">
+                </a>
+            </div>
+        </div>
+        <p>BeerLover, votre cave à bières numérique - © <?php echo date("Y")?>
+            <a href="#">Politique de confidentialité</a></p>
+    </footer>
     </body>
 </html>
